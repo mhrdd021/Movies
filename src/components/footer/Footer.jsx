33c16586ae2996eefb37b42739de374986893518
mhrdd <mhrddmhrdd5610mhrddmhrdd@gmail.com>
2023-04-11 13:40:02 +0430

    linking Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -17,74 +17,83 @@ const Footer = () => {
       className="flex-row sm:flex items-center p-8 w-full h-auto bg-cover bg-no-repeat text-slate-100 relative bottom-0"
       style={{ backgroundImage: `url(${FooterLogo})` }}
     >
-
-        <div className="w-full md:w-1/4">
-          <div className="flex items-center justify-center flex-col gap-3">
-            <h1 className="text-2xl font-bold">Movie Site</h1>
-            <p className="text-slate-300">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas
-              purus viverra accumsan in nisl nisi Arcu cursus vitae congue
-              mauris rhoncus aenean vel elit scelerisque In egestas erat
-
-            </p>
-          </div>
+      <div className="w-full md:w-1/4">
+        <div className="flex items-center justify-center flex-col gap-3">
+          <h1 className="text-2xl font-bold">Movie Site</h1>
+          <p className="text-slate-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas
+            purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris
+            rhoncus aenean vel elit scelerisque In egestas erat
+          </p>
         </div>
+      </div>
 
-        <div className="w-full my-5 md:my-auto md:w-1/2 flex items-center justify-evenly">
-          <div className="w-full md:w-1/2">
-            <ul className="flex items-center justify-center flex-col gap-3">
-              <li className="text-xl font-bold ">Links</li>
-              <Link to={'/'}>
-                <li>Home Page</li>
-              </Link>
-              <li>Movies</li>
-              <Link to={'/categories'}>
-                <li>Category</li>
-              </Link>
-              <li>Sign Up</li>
-              <li>About Us</li>
-            </ul>
-          </div>
-
-          <div className="w-full md:w-1/2">
-            <ul className="flex items-center justify-center flex-col gap-3">
-              <li className="text-xl font-bold ">Links</li>
+      <div className="w-full my-5 md:my-auto md:w-1/2 flex items-center justify-evenly">
+        <div className="w-full md:w-1/2">
+          <ul className="flex items-center justify-center flex-col gap-3">
+            <li className="text-xl font-bold ">Links</li>
+            <Link to={"/"}>
               <li>Home Page</li>
-              <li>Movies</li>
+            </Link>
+            <li>Movies</li>
+            <Link to={"/categories"}>
               <li>Category</li>
+            </Link>
+            <Link to={"/SignUp"}>
               <li>Sign Up</li>
+            </Link>
+            <Link to={"/AboutUs"}>
               <li>About Us</li>
-            </ul>
-          </div>
+            </Link>
+          </ul>
         </div>
 
-        <div className="w-full md:w-1/4">
-          <div className="flex items-center justify-center flex-col relative top-0 gap-3">
-            <h1 className="text-xl font-bold">Contact us</h1>
-            <input
-              className="outline-none border-none text-slate-900 rounded-xl w-full px-2 py-1.5 mb-2"
-              placeholder="Email Addresses"
-            />
-            <div>
-              <div className="flex items-center justify-start gap-4 my-1.5">
-                <SlLocationPin className="text-white" />
-                <span>Tehran-Tajrish avenue - Sabet alley</span>
-              </div>
+        <div className="w-full md:w-1/2">
+          <ul className="flex items-center justify-center flex-col gap-3">
+            <li className="text-xl font-bold ">Pages</li>
+            <Link to={"/"}>
+              <li>Home Page</li>
+            </Link>
+            <li>Movies</li>
+            <Link to={"/categories"}>
+              <li>Category</li>
+            </Link>
+            <Link to={"/SignUp"}>
+              <li>Sign Up</li>
+            </Link>
+            <Link to={"/AboutUs"}>
+              <li>About Us</li>
+            </Link>
+          </ul>
+        </div>
+      </div>
 
-              <div className="flex items-center justify-start gap-4 my-1.5">
-                <BsTelephone className="text-white" />
-                <span>09123456789</span>
-              </div>
+      <div className="w-full md:w-1/4">
+        <div className="flex items-center justify-center flex-col relative top-0 gap-3">
+          <h1 className="text-xl font-bold">Contact us</h1>
+          <input
+            className="outline-none border-none text-slate-900 rounded-xl w-full px-2 py-1.5 mb-2"
+            placeholder="Email Addresses"
+          />
+          <div>
+            <div className="flex items-center justify-start gap-4 my-1.5">
+              <SlLocationPin className="text-white" />
+              <span>Tehran-Tajrish avenue - Sabet alley</span>
+            </div>
 
-              <div className="flex items-center justify-start gap-4 my-1.5">
-                <FiMail className="text-white" />
-                <span>mhrddmhrdd0@gmail.com</span>
-              </div>
+            <div className="flex items-center justify-start gap-4 my-1.5">
+              <BsTelephone className="text-white" />
+              <span>09123456789</span>
+            </div>
+
+            <div className="flex items-center justify-start gap-4 my-1.5">
+              <FiMail className="text-white" />
+              <span>mhrddmhrdd0@gmail.com</span>
             </div>
           </div>
         </div>
-
+      </div>
     </div>
   );
 };
--- src/pages/movies/AllMoviesCart.jsx
+++ src/pages/movies/AllMoviesCart.jsx
@@ -14,7 +14,7 @@ const AllMoviesCart = ({ data }) => {
   }
 
   return (
-    <Link to={`/movie/${data.id}`}>
+    <>
       <div
         className="w-full h-72 md:h-64 bg-cover bg-no-repeat bg-center rounded-lg"
         style={{ backgroundImage: `url(${data.poster})` }}
@@ -35,16 +35,18 @@ const AllMoviesCart = ({ data }) => {
             </h3>
             <h3>Country : {data.country}</h3>
             <h3>Year : {data.year}</h3>
-            <button className="flex item-center justify-center mx-auto w-fit py-1 px-2 rounded-full bg-red-600 text-slate-100 text-sm font-bold my-2 shadow-md shadow-red-600 hover:scale-105 active:scale-95 duration-300">
-              Watch Now
-            </button>
+            <Link to={`/movie/${data.id}`}>
+              <button className="flex item-center justify-center mx-auto w-fit py-1 px-2 rounded-full bg-red-600 text-slate-100 text-sm font-bold my-2 shadow-md shadow-red-600 hover:scale-105 active:scale-95 duration-300">
+                Watch Now
+              </button>
+            </Link>
           </div>
         </div>
       </div>
       <h3 className="flex item-center justify-center text-slate-100 text-sm font-semibold py-2 text-center">
         {data.title}
       </h3>
-    </Link>
+    </>
   );
 };
 
